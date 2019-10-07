abd0440f152786eec1924a5110c60e7ae79b6341
Alex <alexander98mangus@gmail.com>
2019-10-07 17:09:53 +0300

    feat: multiplication processing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function multiply(first, second) {
-  // your solution
+    let bigIntFirst  = BigInt(first);  
+    let bigIntSecond = BigInt(second);
+
+    let multiplication = bigIntFirst * bigIntSecond;
+    let result = String(multiplication);
+    let number = String(result.match(/\d+/g));
+    return number;
 }
